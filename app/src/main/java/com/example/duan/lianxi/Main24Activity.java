a0e8838489b5e826afe24cd1565e02b3e3f10d05
dcz <857999639@qq.com>
2017-05-17 16:59:02 -0700

    s

--- app/src/main/java/com/example/duan/lianxi/Main24Activity.java
+++ app/src/main/java/com/example/duan/lianxi/Main24Activity.java
@@ -1,24 +1,107 @@
 package com.example.duan.lianxi;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import chenjin.BannerActivity;
+import chenjin.GradationScrollView;
 import chenjin.StatusBarUtil;
-import litepal.BaseActivity;
 /**
  * 沉浸式标题
  * styleable.xml
  * */
-public class Main24Activity extends Activity {
+public class Main24Activity extends Activity implements GradationScrollView.ScrollViewListener{
+    private GradationScrollView scrollView;
 
+    private ListView listView;
+
+    private TextView textView;
+    private int height;
+    private ImageView ivBanner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        StatusBarUtil.setImgTransparent(this);
+        StatusBarUtil.setImgTransparent(this);      //这两行是让标题沉浸
         setContentView(R.layout.activity_main24);
+        scrollView = (GradationScrollView) findViewById(R.id.scrollview);
+        listView = (ListView) findViewById(R.id.listview);
+        textView = (TextView) findViewById(R.id.textview);
+        ivBanner = (ImageView) findViewById(R.id.iv_banner);
+
+        ivBanner.setFocusable(true);
+        ivBanner.setFocusableInTouchMode(true);
+        ivBanner.requestFocus();
+
+        initListeners();
+        initData();
+    }
+
+
+    /**
+     * 获取顶部图片高度后，设置滚动监听
+     */
+    private void initListeners() {
+
+        ViewTreeObserver vto = ivBanner.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                textView.getViewTreeObserver().removeGlobalOnLayoutListener(
+                        this);
+                height = ivBanner.getHeight();
+
+                scrollView.setScrollViewListener(Main24Activity.this);
+            }
+        });
+    }
+
+
+
+    private void initData() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Main24Activity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.data));
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent=new Intent(Main24Activity.this, BannerActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    /**
+     * 滑动监听
+     * @param scrollView
+     * @param x
+     * @param y
+     * @param oldx
+     * @param oldy
+     */
+    @Override
+    public void onScrollChanged(GradationScrollView scrollView, int x, int y,
+                                int oldx, int oldy) {
+        // TODO Auto-generated method stub
+        if (y <= 0) {   //设置标题的背景颜色
+            textView.setBackgroundColor(Color.argb((int) 0, 144,151,166));
+        } else if (y > 0 && y <= height) { //滑动距离小于banner图的高度时，设置背景和字体颜色颜色透明度渐变
+            float scale = (float) y / height;
+            float alpha = (255 * scale);
+            textView.setTextColor(Color.argb((int) alpha, 255,255,255));
+            textView.setBackgroundColor(Color.argb((int) alpha, 144,151,166));
+        } else {    //滑动到banner下面设置普通颜色
+            textView.setBackgroundColor(Color.argb((int) 255, 144,151,166));
+        }
     }
 }
