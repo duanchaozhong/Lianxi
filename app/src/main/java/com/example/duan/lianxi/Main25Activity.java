b5e99e8483035e18a44204e527fe9468676dc70d
dcz <857999639@qq.com>
2017-06-15 15:43:04 -0700

    a

--- app/src/main/java/com/example/duan/lianxi/Main25Activity.java
+++ app/src/main/java/com/example/duan/lianxi/Main25Activity.java
@@ -13,6 +13,8 @@ import android.widget.Button;
 
 import java.util.Locale;
 
+import de.greenrobot.event.EventBus;
+
 /**
  * 语言切换
  * 需要加中英文语言包values-en和values-zh
@@ -20,6 +22,7 @@ import java.util.Locale;
 public class Main25Activity extends Activity {
     private Button b1;
     private Button b2;
+    private String EVENT_REFRESH_LANGUAGE="B";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,10 +44,15 @@ public class Main25Activity extends Activity {
                 config.locale = Locale.ENGLISH;
                 getBaseContext().getResources().updateConfiguration(config
                         , getBaseContext().getResources().getDisplayMetrics());
-                Intent intent = new Intent(getApplicationContext(),Main25Activity.class);
+                /*Intent intent = new Intent(getApplicationContext(),Main25Activity.class);
                 intent.setClass(getApplicationContext(),Main25Activity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);*/
+                // 杀掉进程
+              /*  android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(0);*/
+                recreate();//刷新页面
             }
         });
         /**
@@ -72,5 +80,41 @@ public class Main25Activity extends Activity {
         DisplayMetrics dm = resources.getDisplayMetrics();// 获得屏幕参数：主要是分辨率，像素等。
         config.locale = locale; // 简体中文
         resources.updateConfiguration(config, dm);
+    }
+    /**
+     * 变换语言并且保存在本地
+     * */
+    public void changeAppLanguage() {
+     /*   String sta = Store.getLanuageIsChinese() ? "zh" : "en";//这是SharedPreferences工具类，用于保存设置，代码很简单，自己实现吧
+        // 本地语言设置
+        Locale myLocale = new Locale(sta);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);*/
+    }
+
+    /**
+     * EventBus跟广播差不多
+     * */
+    private void demo(){
+        EventBus.getDefault().register(this);   //注册广播
+        EventBus.getDefault().post(EVENT_REFRESH_LANGUAGE);//发送消息
+    }
+/*    //得到消息
+    @Subscribe
+    public void onEvent(String str) {
+        switch (str) {
+        case "B":
+                changeAppLanguage();
+                recreate();//刷新界面
+                break;
+        }
+    }*/
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
     }
 }
--- app/src/main/java/com/example/duan/lianxi/Main8Activity.java
+++ app/src/main/java/com/example/duan/lianxi/Main8Activity.java
@@ -1,12 +1,14 @@
 package com.example.duan.lianxi;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -30,15 +32,18 @@ public class Main8Activity extends Activity {
         String sss = "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈";
         SpannableString spanttt = new SpannableString(ttt);
         SpannableString spansss = new SpannableString(sss);
-        ClickableSpan clickttt = new ShuoMClickableSpan(ttt, this,"1");
-        ClickableSpan clicksss = new ShuoMClickableSpan(ttt, this,"0");
-        spanttt.setSpan(clickttt, 0, ttt.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+       // ClickableSpan clickttt = new ShuoMClickableSpan(ttt, this,"1");
+        ClickableSpan clicksss = new ShuoMClickableSpan(ttt, this,"1");
+        //可以直接设置字体颜色，不需要字体响应
+        spanttt.setSpan(new ForegroundColorSpan(Color.parseColor("#005399")), 0, ttt.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        //设置字体颜色并响应
         spansss.setSpan(clicksss, 0, sss.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
         tv.setText("哈哈");
         tv.append(spanttt);
         tv.append("你是谁你谁你是谁你谁你是谁你谁");
         tv.append(spansss);
         tv.append("你是谁你谁你是谁你谁你是谁你谁你是谁你谁你是谁你谁");
+        tv.setHighlightColor(Color.TRANSPARENT);//设置点击后的颜色为透明，否则会一直出现高亮
         tv.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
